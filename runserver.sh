95ce650c9d2054204278c65444324580005177aa
EC2 Default User <ec2-user@ip-172-31-93-209.ec2.internal>
2025-05-07 14:25:43 +0800

    add release mode in sh

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-echo "Starting server..." >> startup.log
-/usr/local/go/bin/go run ./server/ >> server.log 2>&1
-
+export GIN_MODE=release
+go run ./server/ >> server.log 2>&1
